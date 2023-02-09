07399602328701d5047c96b1c66e749ec3b1299e
Cansuoztas <fcansuoztas@gmail.com>
2023-02-09 10:00:19 +0300

    first

---
+++ app.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: "#app",
+    data:{
+        player_heal: 100,
+        mounster_heal: 100,
+
+    },
+    methods :{
+        start_game: function(){
+            
+        },
+        attack: function(){
+
+        },
+        special_attack: function(){
+
+        },
+        heal_up: function(){
+
+        },
+        give_up: function(){
+
+        }
+    }
+
+})
